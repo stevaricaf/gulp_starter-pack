8c153e05a4dcc741a001008146d18595af34a658
Stevan Stojić <41612176+stevaricaf@users.noreply.github.com>
2020-09-14 16:06:05 +0200

    Update gulpfile.js
    Added js dependencies and js tasks to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,22 +11,47 @@ let {
 }                   = require('gulp'),
     dustJs          = require('dustjs-linkedin'),
     dustHtml        = require('gulp-dust-html'),
-    htmlBeautify    = require('gulp-html-beautify');
+    htmlBeautify    = require('gulp-html-beautify'),
+    rename          = require('gulp-rename'),
+    notify          = require('gulp-notify'),
+    plumber         = require('gulp-plumber'),
+    sourcemaps      = require('gulp-sourcemaps'),
+    browserify      = require('browserify'),
+    source          = require('vinyl-source-stream'),
+    buffer          = require('vinyl-buffer'),
+    babel           = require('gulp-babel'),
+    concat          = require('gulp-concat'),
+    uglify          = require('gulp-uglify-es').default,
+    clean           = require('gulp-clean');
 
 // ---------- Initializing file destinations ---------- //
 
 let paths = {
     // HTML templates
-    templates: {
+    html: {
         dir: './templates/pages/',
         dest: './'
+    },
+    // JavaScript
+    js: {
+        dir: './js/',
+        dest: './src/js/'
     }
+};
+
+// ---------- Error notification ---------- //
+
+const errorNotification = {
+    errorHandler: notify.onError({
+        title: "Ops, you've made a mistake, find it...",
+        message: 'Error: <%= error.message %>'
+    })
 };
 
 // ---------- HTML templates task ---------- //
 
 function htmlTask() {
-    return src(paths.templates.dir + '**/*.html')
+    return src(paths.html.dir + '**/*.html')
         .pipe(dustHtml({
             basePath: 'templates',
             data: {},
@@ -43,22 +68,73 @@ function htmlTask() {
             preserve_newlines: false,
             end_with_newline: false
         }))
-        .pipe(dest(paths.templates.dest))
+        .pipe(dest(paths.html.dest))
+};
+
+// ---------- JavaScript tasks ---------- //
+
+function jsMainTask() {
+    return browserify(paths.js.dir + 'main.js').bundle()
+        .pipe(source('main.js'))
+        .pipe(buffer())
+        .pipe(babel({
+            compact: false,
+            comments: false
+        }))
+        .pipe(dest(paths.js.dest))
+};
+
+function jsLibsTask() {
+    return src(paths.js.dest + 'libs/**/*.js')
+        .pipe(concat('libs.js'))
+        .pipe(dest(paths.js.dest))
+};
+
+function jsMergeTask() {
+    return src([paths.js.dest + 'libs.js', paths.js.dest + 'main.js'], { 
+            allowEmpty: true 
+        })
+        .pipe(plumber(errorNotification))
+        .pipe(sourcemaps.init())
+        .pipe(concat('global.js'))
+        .pipe(dest(paths.js.dest))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest(paths.js.dest + 'dist'))
+};
+
+function jsCleanTask() {
+    return src([paths.js.dest + 'libs.js', paths.js.dest + 'main.js'], {
+            allowEmpty: true,
+            read: false
+        })
+        .pipe(clean())
 };
 
 // ---------- Define watch tasks ---------- //
 
 function watchTask() {
     // Watch HTML files
-    watch([paths.templates.dir + '**/*.html', '!templates/*.html'], htmlTask);
+    watch([paths.html.dir + '**/*.html', '!templates/*.html'], htmlTask);
+    // Watch JS files
+    watch([paths.js.dir + '**/*.js', !paths.js.dir + 'libs/**/*.js'], jsBuild);
 };
 
 // ---------- Define complex tasks ---------- //
 
-let build = series(htmlTask, watchTask);
+let jsBuild     = series(jsMainTask, jsLibsTask, jsMergeTask, jsCleanTask);
+let build       = series(parallel(htmlTask, jsBuild), watchTask);
 
 // ---------- Exports tasks ---------- //
 
 exports.html        = htmlTask;
+exports.jsMain      = jsMainTask;
+exports.jsLibs      = jsLibsTask;
+exports.jsMerge     = jsMergeTask;
+exports.jsClean     = jsCleanTask;
 exports.watch       = watchTask;
+exports.jsBuild     = jsBuild;
 exports.default     = build;
