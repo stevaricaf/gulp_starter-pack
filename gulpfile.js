4e49a00fa5c12c57f256925cc26cb323754132bb
Stevan Stojić <41612176+stevaricaf@users.noreply.github.com>
2020-09-14 13:34:31 +0200

    Update gulpfile.js
    Added html dependencies and html task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,49 @@ let {
     watch,
     series,
     parallel
-}                   = require('gulp');
+}                   = require('gulp'),
+    dustJs          = require('dustjs-linkedin'),
+    dustHtml        = require('gulp-dust-html'),
+    htmlBeautify    = require('gulp-html-beautify');
 
-exports.default;
+// ---------- Initializing file destinations ---------- //
+
+let paths = {
+    // Html templates
+    templates: {
+        dir: './templates/pages/',
+        dest: './'
+    }
+};
+
+// ---------- HTML templates task ---------- //
+
+function htmlTask() {
+    return src(paths.templates.dir + '**/*.html')
+        .pipe(dustHtml({
+            basePath: 'templates',
+            data: {},
+            whitespace: true,
+            defaultExt: '.html',
+            config: {
+                cache: false
+            }
+        }))
+        .pipe(htmlBeautify({
+            indent_size: 4,
+            indent_char: '',
+            indent_with_tabs: true,
+            preserve_newlines: false,
+            end_with_newline: false
+        }))
+        .pipe(dest(paths.templates.dest))
+};
+
+// ---------- Define complex tasks ---------- //
+
+let build = htmlTask;
+
+// ---------- Exports tasks ---------- //
+
+exports.html        = htmlTask;
+exports.default     = build;
